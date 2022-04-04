1e94b983d4ae6a625246507e8fcf3c764e0e97e3
muhsufyan <muh.sufyan.ts@gmail.com>
2022-04-04 13:27:35 +0700

    menangkap data dari client lalu tampilkan di browser menggunakan body parser

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # typescript-express-dasar
 <ul>
-    <li>yarn add express untuk deploy production untuk development yarn add express -D <br>untuk deploy production pd package.json disimpan pd dependencies sedangkan deploy development ada di devDependencies</li>
-    <li>agar import express from 'express' dpt berjln install "yarn add @types/express -D" <br>hal diatas agar dpt import express dan ini termasuk kedlm development hanya perlu di development saja</li>
+    <li>perlu data parse untuk parser data dari client jd format json<br> yarn add body-parser <br> {
+ "title": "hey tayo",
+ "image": "gambar"
+}</li>
 </ul>
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,29 @@
-// CARA LAMA MENGGUNAKAN JS
-// import express from 'express'
-// const app = express();
-// app.route("/").get((response,request)=>{
-//     response.send("halo");
-// });
-
-// app.listen(8000);
-// CARA 2 DENGAN TYPESCRIPT
-// interface dr express
 import express, { Application, Request, Response } from 'express'; 
+import bodyParser from 'body-parser';
 class App{
     // buat variabel global app yg bertipe Application
     public app: Application;
+    // convert jd json dg body parser
+    protected plugins(): void{
+        // use untuk menggunakan/menerapkan suatu method/middleware/library ke app
+        this.app.use(bodyParser.json());
+    }
     // buat routernya
     protected routes(): void{
         // karena didlm class kita pakai this untuk variabel global app diatas
         this.app.route("/").get((req: Request, res: Response)=>{
-            res.send("route ini ditulis dengan typescript")
+            res.send("route get ditulis dlm typescript")
+        }),
+        // melihat param req pada post
+        this.app.route("/").post((req: Request, res: Response)=>{
+            // cetak param yg dikirim client
+            res.send(req.body)
         })
     }
     // buat constructor untuk Application agar tipe data Application dpt digunakan
     constructor(){
         this.app = express();
+        this.plugins();
         // panggil route yg tlh dibuat diatas
         this.routes();
     }
